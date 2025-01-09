3479f2b5d8b4367e676a9261a7060ebcb34df7bd
ViscousPot <viscouspotential@gmail.com>
2025-01-09 21:19:43 +0000

    fix: store hascontributes separate from repos

--- app/src/main/java/com/viscouspot/gitsync/util/SettingsManager.kt
+++ app/src/main/java/com/viscouspot/gitsync/util/SettingsManager.kt
@@ -75,16 +75,14 @@ class SettingsManager internal constructor(private val context: Context, private
     private fun reloadSharedPref() {
         val newIndex = repoIndex ?: repoManager.getRepoIndex()
         val repoNames = repoManager.getRepoNames()
-        log("getSharedPref")
-        log(currentRepoIndex)
-        log(newIndex)
-        log(repoManager.getRepoNames())
+
         if (currentRepoIndex == newIndex && currentRepoNames[newIndex] == repoNames[newIndex]) {
             return
         }
 
         currentRepoIndex = newIndex
         currentRepoNames = repoNames
+
         settingsSharedPref = EncryptedSharedPreferences.create(
             context,
             "${PREFIX}${repoManager.getRepoNames().elementAt(newIndex)}",
@@ -336,21 +334,6 @@ class SettingsManager internal constructor(private val context: Context, private
 
         with(settingsSharedPref.edit()) {
             putBoolean("syncOnAppClosed", enabled)
-            apply()
-        }
-    }
-
-    fun hasContributed(): Boolean {
-        reloadSharedPref()
-
-        return settingsSharedPref.getBoolean("hasContributed", false)
-    }
-
-    fun setHasContributed() {
-        reloadSharedPref()
-
-        with(settingsSharedPref.edit()) {
-            putBoolean("hasContributed", true)
             apply()
         }
     }
