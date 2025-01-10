43ba930ffccc27bf84e83bb8313a34008363f4fb
ViscousPot <viscouspotential@gmail.com>
2025-01-10 08:51:42 +0000

    bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.viscouspot.gitsync"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1643
-        versionName  = "1.643"
+        versionCode = 1644
+        versionName  = "1.644"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
