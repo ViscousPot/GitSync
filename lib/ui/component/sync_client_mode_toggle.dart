eaa647113d4603a24bfe61ce5852c8c10ac96227
ViscousPot <viscouspotential@gmail.com>
2025-12-12 13:10:17 +0000

    update: sync/client toggle ui animations

--- lib/ui/component/sync_client_mode_toggle.dart
+++ lib/ui/component/sync_client_mode_toggle.dart
@@ -22,119 +22,151 @@ class _SyncClientModeToggleState extends State<SyncClientModeToggle> {
       builder: (context, clientModeEnabledSnapshot) => Row(
         children: [
           Expanded(
-            child: TextButton.icon(
-              onPressed: () async {
-                if (widget.global) {
-                  await repoManager.setBool(StorageKey.repoman_defaultClientModeEnabled, false);
-                } else {
-                  await uiSettingsManager.setBoolNullable(StorageKey.setman_clientModeEnabled, false);
-                }
-                setState(() {});
-              },
-              style: ButtonStyle(
-                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                padding: WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: spaceSM, horizontal: spaceMD)),
-                backgroundColor: WidgetStatePropertyAll(clientModeEnabledSnapshot.data != true ? tertiaryInfo : tertiaryDark),
-                shape: WidgetStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: cornerRadiusMD,
-                      topRight: Radius.zero,
-                      bottomLeft: cornerRadiusMD,
-                      bottomRight: Radius.zero,
-                    ),
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 200),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(topLeft: cornerRadiusMD, topRight: Radius.zero, bottomLeft: cornerRadiusMD, bottomRight: Radius.zero),
+                color: clientModeEnabledSnapshot.data != true ? tertiaryInfo : tertiaryDark,
+              ),
+              child: TextButton.icon(
+                onPressed: () async {
+                  if (widget.global) {
+                    await repoManager.setBool(StorageKey.repoman_defaultClientModeEnabled, false);
+                  } else {
+                    await uiSettingsManager.setBoolNullable(StorageKey.setman_clientModeEnabled, false);
+                  }
+                  setState(() {});
+                },
+                style: ButtonStyle(
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  padding: WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: spaceSM, horizontal: spaceMD)),
+                  backgroundColor: WidgetStatePropertyAll(clientModeEnabledSnapshot.data != true ? tertiaryInfo : tertiaryDark),
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: cornerRadiusMD,
+                        topRight: Radius.zero,
+                        bottomLeft: cornerRadiusMD,
+                        bottomRight: Radius.zero,
+                      ),
 
-                    side: clientModeEnabledSnapshot.data != true ? BorderSide.none : BorderSide(width: 3, color: tertiaryInfo),
+                      side: clientModeEnabledSnapshot.data != true ? BorderSide.none : BorderSide(width: 3, color: tertiaryInfo),
+                    ),
                   ),
                 ),
-                animationDuration: Duration.zero,
-              ),
-              icon: FaIcon(FontAwesomeIcons.arrowsRotate, color: clientModeEnabledSnapshot.data != true ? tertiaryDark : primaryLight, size: textMD),
-              label: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      t.syncMode,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: clientModeEnabledSnapshot.data != true ? tertiaryDark : primaryLight,
-                        fontSize: textMD,
-                        fontWeight: FontWeight.bold,
+                icon: FaIcon(
+                  FontAwesomeIcons.arrowsRotate,
+                  color: clientModeEnabledSnapshot.data != true ? tertiaryDark : primaryLight,
+                  size: textMD,
+                ),
+                label: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AnimatedDefaultTextStyle(
+                        child: Text(
+                          t.syncMode,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: textMD, fontWeight: FontWeight.bold),
+                        ),
+                        style: TextStyle(
+                          color: clientModeEnabledSnapshot.data != true ? tertiaryDark : primaryLight,
+                          fontSize: textMD,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        duration: Duration(milliseconds: 200),
                       ),
-                    ),
-                    SizedBox(height: spaceXXXXS),
-                    Text(
-                      t.syncModeDescription,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: clientModeEnabledSnapshot.data != true ? tertiaryDark : primaryLight,
-                        fontSize: textXS,
-                        fontWeight: FontWeight.bold,
+                      SizedBox(height: spaceXXXXS),
+                      AnimatedDefaultTextStyle(
+                        child: Text(
+                          t.syncModeDescription,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: textXS, fontWeight: FontWeight.bold),
+                        ),
+                        style: TextStyle(
+                          color: clientModeEnabledSnapshot.data != true ? tertiaryDark : primaryLight,
+                          fontSize: textMD,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        duration: Duration(milliseconds: 200),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
           Expanded(
-            child: TextButton.icon(
-              onPressed: () async {
-                if (widget.global) {
-                  await repoManager.setBool(StorageKey.repoman_defaultClientModeEnabled, true);
-                } else {
-                  await uiSettingsManager.setBoolNullable(StorageKey.setman_clientModeEnabled, true);
-                }
-                setState(() {});
-              },
-              style: ButtonStyle(
-                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                padding: WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: spaceSM, horizontal: spaceMD)),
-                backgroundColor: WidgetStatePropertyAll(clientModeEnabledSnapshot.data == true ? tertiaryInfo : tertiaryDark),
-                shape: WidgetStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.zero,
-                      topRight: cornerRadiusMD,
-                      bottomLeft: Radius.zero,
-                      bottomRight: cornerRadiusMD,
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 200),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(topLeft: Radius.zero, topRight: cornerRadiusMD, bottomLeft: Radius.zero, bottomRight: cornerRadiusMD),
+                color: clientModeEnabledSnapshot.data == true ? tertiaryInfo : tertiaryDark,
+              ),
+              child: TextButton.icon(
+                onPressed: () async {
+                  if (widget.global) {
+                    await repoManager.setBool(StorageKey.repoman_defaultClientModeEnabled, true);
+                  } else {
+                    await uiSettingsManager.setBoolNullable(StorageKey.setman_clientModeEnabled, true);
+                  }
+                  setState(() {});
+                },
+                style: ButtonStyle(
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  padding: WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: spaceSM, horizontal: spaceMD)),
+                  backgroundColor: WidgetStatePropertyAll(clientModeEnabledSnapshot.data == true ? tertiaryInfo : tertiaryDark),
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.zero,
+                        topRight: cornerRadiusMD,
+                        bottomLeft: Radius.zero,
+                        bottomRight: cornerRadiusMD,
+                      ),
+                      side: clientModeEnabledSnapshot.data == true ? BorderSide.none : BorderSide(width: 3, color: tertiaryInfo),
                     ),
-                    side: clientModeEnabledSnapshot.data == true ? BorderSide.none : BorderSide(width: 3, color: tertiaryInfo),
                   ),
                 ),
-                animationDuration: Duration.zero,
-              ),
-              iconAlignment: IconAlignment.end,
-              icon: FaIcon(FontAwesomeIcons.codeCompare, color: clientModeEnabledSnapshot.data == true ? tertiaryDark : primaryLight, size: textMD),
-              label: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      t.clientMode,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: clientModeEnabledSnapshot.data == true ? tertiaryDark : primaryLight,
-                        fontSize: textMD,
-                        fontWeight: FontWeight.bold,
+                iconAlignment: IconAlignment.end,
+                icon: FaIcon(FontAwesomeIcons.codeCompare, color: clientModeEnabledSnapshot.data == true ? tertiaryDark : primaryLight, size: textMD),
+                label: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AnimatedDefaultTextStyle(
+                        child: Text(
+                          t.clientMode,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: textMD, fontWeight: FontWeight.bold),
+                        ),
+                        style: TextStyle(
+                          color: clientModeEnabledSnapshot.data == true ? tertiaryDark : primaryLight,
+                          fontSize: textMD,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        duration: Duration(milliseconds: 200),
                       ),
-                    ),
-                    SizedBox(height: spaceXXXXS),
-                    Text(
-                      t.clientModeDescription,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: clientModeEnabledSnapshot.data == true ? tertiaryDark : primaryLight,
-                        fontSize: textXS,
-                        fontWeight: FontWeight.bold,
+                      SizedBox(height: spaceXXXXS),
+                      AnimatedDefaultTextStyle(
+                        child: Text(
+                          t.clientModeDescription,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: textXS, fontWeight: FontWeight.bold),
+                        ),
+                        style: TextStyle(
+                          color: clientModeEnabledSnapshot.data == true ? tertiaryDark : primaryLight,
+                          fontSize: textXS,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        duration: Duration(milliseconds: 200),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
