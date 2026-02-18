fba90037a5efd5eb95c2ae9cc830760941fee0dc
ViscousPot <viscouspotential@gmail.com>
2026-02-18 22:23:53 +0000

    improve: android widget icons

--- android/app/src/main/kotlin/com/viscouspot/gitsync/widget/ManualSyncWidget.kt
+++ android/app/src/main/kotlin/com/viscouspot/gitsync/widget/ManualSyncWidget.kt
@@ -34,6 +34,7 @@ import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
+import androidx.glance.layout.size
 import androidx.glance.state.GlanceStateDefinition
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
@@ -92,11 +93,11 @@ class ManualSyncWidget : GlanceAppWidget() {
             verticalAlignment = Alignment.CenterVertically
         ) {
             Box(
-                modifier = GlanceModifier.padding(end = if (showShortText) 16.dp else 0.dp),
+                modifier = GlanceModifier.size(48.dp).padding(end = if (showShortText) 16.dp else 0.dp),
                 contentAlignment = Alignment.Center
             ) {
                 Image(
-                    provider = ImageProvider(R.drawable.manual_sync_small),
+                    provider = ImageProvider(R.drawable.manual_sync),
                     contentDescription = "Force Sync",
                     colorFilter = ColorFilter.tint(ColorProvider(Color.White)),
                     contentScale = ContentScale.Fit
