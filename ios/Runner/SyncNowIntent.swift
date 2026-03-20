be5bcd567104519054651eefb31a821758e103c5
ViscousPot <viscouspotential@gmail.com>
2026-03-20 20:21:16 +0000

    feat: ios shortcuts support

---
+++ ios/Runner/SyncNowIntent.swift
@@ -0,0 +1,24 @@
+import AppIntents
+import Foundation
+import home_widget
+
+@available(iOS 17, *)
+struct SyncNowIntent: AppIntent {
+    static var title: LocalizedStringResource = "Sync Now"
+    static var description: IntentDescription? = IntentDescription(
+        "Synchronize the currently selected repository")
+    static var openAppWhenRun: Bool = true
+    static var isDiscoverable: Bool = true
+
+    func perform() async throws -> some IntentResult {
+        let syncUrl = URL(string: "gitsync://sync-now")!
+
+        await HomeWidgetBackgroundWorker.run(url: syncUrl, appGroup: "group.ForceSyncWidget")
+
+        return .result()
+    }
+}
+
+@available(iOS 17, *)
+@available(iOSApplicationExtension, unavailable)
+extension SyncNowIntent: ForegroundContinuableIntent {}
